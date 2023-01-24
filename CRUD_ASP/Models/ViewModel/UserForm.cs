c15152d3b5ecae80c2ba1307c5996304858927f3
michael verminnen <verminnenmichael@gmail.com>
2023-01-24 02:56:46 +0100

    Le formulaire est fait mais avec les autres modif pour qu'il fonctionne

---
+++ CRUD_ASP/Models/ViewModel/UserForm.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Build.Framework;
+
+namespace CRUD_ASP.Models.ViewModel
+{
+    public class UserForm
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
